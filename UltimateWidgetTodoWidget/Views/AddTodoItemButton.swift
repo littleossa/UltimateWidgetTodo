436f36a4b3531a0dda99c4475acfbae19ff02610
littleossa <littleossa@gmail.com>
2023-11-01 08:22:29 +0900

    Add AddTodoItemButton

---
+++ UltimateWidgetTodoWidget/Views/AddTodoItemButton.swift
@@ -0,0 +1,47 @@
+//
+//  AddButton.swift
+//  UltimateWidgetTodo
+//
+//
+
+import AppIntents
+import WidgetKit
+import SwiftUI
+
+struct AddTodoItemButton: View {
+            
+    var body: some View {
+        
+        Button(intent: AddTodoItemButtonIntent()) {
+            ZStack {
+                Image(systemName: "circle.fill")
+                    .resizable()
+                    .foregroundStyle(.widgetBackground)
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .foregroundStyle(.blue)
+            }
+        }
+    }
+}
+
+struct AddTodoItemButtonIntent: AppIntent {
+    
+    static var title: LocalizedStringResource = "Tap Add button"
+    
+    @Parameter(title: "tapAddButton")
+    var id: String
+    
+    init() {}
+    
+    func perform() async throws -> some IntentResult {
+        // TODO: Do something
+        return .result()
+    }
+}
+
+#Preview(as: .systemLarge) {
+    TodoListPreviewWidget()
+} timeline: {
+    TodoItemEntry(date: .now, emoji: "😀")
+}
