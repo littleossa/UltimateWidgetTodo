af4e87d257f6338abdd8839bbc7d54e773ec7bcd
littleossa <littleossa@gmail.com>
2023-10-31 23:38:11 +0900

    Update TodoListView

--- UltimateWidgetTodoWidget/Views/TodoListView.swift
+++ UltimateWidgetTodoWidget/Views/TodoListView.swift
@@ -4,18 +4,34 @@
 //
 //
 
+import SwiftData
 import SwiftUI
+import WidgetKit
 
-struct TodoListView : View {
+struct TodoListView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @Query (sort: \TodoItem.createDate, order: .forward)
+    private var items: [TodoItem]
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text("Emoji:")
+            ForEach(items) {
+                Text($0.name)
+            }
         }
     }
 }
 
 #Preview {
     TodoListView()
+        .modelContainer(
+            AppModelContainer.testStore.makePreviewContainer(itemCount: 1)
+        )
+        .previewContext(WidgetPreviewContext(family: .systemLarge))
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(.yellow)
+        }
 }
