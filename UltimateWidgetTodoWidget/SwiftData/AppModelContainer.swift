a5beba5dbbd1de92125366affc72a04cd476f905
littleossa <littleossa@gmail.com>
2023-10-31 21:56:49 +0900

    Update: AppModelContainer

---
+++ UltimateWidgetTodoWidget/SwiftData/AppModelContainer.swift
@@ -0,0 +1,35 @@
+//
+//  AppModelContainer.swift
+//  UltimateWidgetTodoWidgetExtension
+//
+//
+
+import SwiftData
+
+class AppModelContainer {
+    
+    private init(isTesting: Bool) {
+        let schema = Schema([
+            Item.self,
+        ])
+        let configuration = ModelConfiguration(schema: schema,
+                                               isStoredInMemoryOnly: isTesting)
+        
+        do {
+            container = try ModelContainer(for: schema,
+                                           configurations: [configuration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+    
+    static let shared = AppModelContainer(isTesting: false)
+    static let testStore = AppModelContainer(isTesting: true)
+    
+    private let container: ModelContainer
+    
+    @MainActor
+    var context: ModelContext {
+        return container.mainContext
+    }
+}
