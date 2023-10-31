60e4ff082ac71a42bf96172b992abe120d4751b2
littleossa <littleossa@gmail.com>
2023-10-31 21:13:57 +0900

    Initial commit

---
+++ UltimateWidgetTodo/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  UltimateWidgetTodo
+//
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ UltimateWidgetTodo/UltimateWidgetTodoApp.swift
@@ -0,0 +1,31 @@
+//
+//  UltimateWidgetTodoApp.swift
+//  UltimateWidgetTodo
+//
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct UltimateWidgetTodoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
