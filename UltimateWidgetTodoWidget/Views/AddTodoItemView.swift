b6b873e25201a22c13f43176a125baefaf4b0db0
littleossa <littleossa@gmail.com>
2023-11-01 21:19:52 +0900

    Add WidgetKeyboard

--- UltimateWidgetTodoWidget/Views/AddTodoItemView.swift
+++ UltimateWidgetTodoWidget/Views/AddTodoItemView.swift
@@ -9,7 +9,10 @@ import WidgetKit
 
 struct AddTodoItemView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            WidgetKeyboard()
+        }
     }
 }
 
---
+++ UltimateWidgetTodoWidget/Views/WidgetKeyboard.swift
@@ -0,0 +1,20 @@
+//
+//  WidgetKeyboard.swift
+//  UltimateWidgetTodoWidgetExtension
+//
+//
+
+import SwiftUI
+import WidgetKit
+
+struct WidgetKeyboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview(as: .systemLarge) {
+    AddTodoItemPreviewWidget()
+} timeline: {
+    TodoItemEntry(date: .now, emoji: "😀")
+}
