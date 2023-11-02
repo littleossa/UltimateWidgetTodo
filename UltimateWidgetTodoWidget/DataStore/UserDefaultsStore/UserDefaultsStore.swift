d698d8853ab0377a6a5eb5cdeeec2d6bd34cf504
littleossa <littleossa@gmail.com>
2023-11-03 07:41:37 +0900

    Add UserDefaultsStore

---
+++ UltimateWidgetTodoWidget/DataStore/UserDefaultsStore/UserDefaultsStore.swift
@@ -0,0 +1,73 @@
+//
+//  UserDefaultsStore.swift
+//  UltimateWidgetTodo
+//
+//
+
+import Foundation
+
+class UserDefaultsStore {
+    
+    static let shared = UserDefaultsStore()
+    static let testStore = UserDefaultsStore(isTesting: true)
+    
+    private init(isTesting: Bool = false) {
+        let suiteName = isTesting ? "testStore" : "widgetTodo"
+        userDefaults = UserDefaults(suiteName: suiteName) ?? UserDefaults()
+    }
+    
+    private let userDefaults: UserDefaults
+    
+    enum Key: String {
+        case inputText
+        case isCapsLocked
+        case isShownKeyboard
+        case keyboardInputMode
+    }
+    
+    var inputText: String {
+        get {
+            return userDefaults.string(forKey: Key.inputText.rawValue) ?? ""
+        }
+        set {
+            userDefaults.set(newValue, forKey: Key.inputText.rawValue)
+        }
+    }
+    
+    var isCapsLocked: Bool {
+        get {
+            return userDefaults.bool(forKey: Key.isCapsLocked.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Key.isCapsLocked.rawValue)
+        }
+    }
+    
+    var isShownKeyboard: Bool {
+        get {
+            return userDefaults.bool(forKey: Key.isShownKeyboard.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Key.isShownKeyboard.rawValue)
+        }
+    }
+    
+    var keyboardInputMode: KeyboardInputMode {
+        get {
+            let value = userDefaults.string(forKey: Key.keyboardInputMode.rawValue) ?? ""
+            if let mode = KeyboardInputMode(rawValue: value) {
+                return mode
+            }
+            return .alphabet
+        }
+        set {
+            userDefaults.set(newValue.rawValue, forKey: Key.keyboardInputMode.rawValue)
+        }
+    }
+}
+
+enum KeyboardInputMode: String {
+    case alphabet
+    case emoji
+    case number
+}
