8592524bf68bdd6552c4631cf13b5db07a0353ea
littleossa <littleossa@gmail.com>
2023-11-06 06:47:34 +0900

    Move Line to other file

---
+++ UltimateWidgetTodoWidget/Views/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  UltimateWidgetTodoWidgetExtension
+//
+//
+
+import SwiftUI
+
+struct Line: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        return path
+    }
+}
--- UltimateWidgetTodoWidget/Views/TodoList/TodoListRow.swift
+++ UltimateWidgetTodoWidget/Views/TodoList/TodoListRow.swift
@@ -31,12 +31,3 @@ struct TodoListRow: View {
         }
     }
 }
-
-struct Line: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: rect.width, y: 0))
-        return path
-    }
-}
