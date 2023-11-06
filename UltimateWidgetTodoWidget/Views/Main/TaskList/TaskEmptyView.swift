05263e705865d264a0b8937d584a65e84abf6b2c
littleossa <littleossa@gmail.com>
2023-11-06 19:02:22 +0900

    Add TaskEmptyView

---
+++ UltimateWidgetTodoWidget/Views/Main/TaskList/TaskEmptyView.swift
@@ -0,0 +1,65 @@
+//
+//  TaskEmptyView.swift
+//  UltimateWidgetTodoWidgetExtension
+//
+//
+
+
+import SwiftUI
+
+struct TaskEmptyView: View {
+        
+    var body: some View {
+        ZStack {
+            VStack(spacing: 32) {
+                
+                Spacer()
+                
+                VStack(spacing: 8) {
+                    Text("What's your task?")
+                        .font(.system(size: 32))
+                        .bold()
+                        .foregroundStyle(Color.black)
+                    
+                    Text("Start by adding a new one.")
+                        .font(.system(size: 20))
+                        .fontWeight(.light)
+                }
+                
+                AddButtonImage()
+                .frame(width: 80, height: 80)
+                
+                Spacer()
+            }
+            AddTaskButton(type: .fullScreen)
+        }
+    }
+}
+
+// MARK: - Preview
+#if DEBUG
+import WidgetKit
+
+struct TaskEmptyPreviewWidget: Widget {
+    let kind: String = "UltimateWidgetTodo"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(
+            kind: kind,
+            provider: WidgetTodoProvider()
+        ) { entry in
+            TaskEmptyView()
+                .containerBackground(for: .widget) {
+                    WidgetBackgroundView()
+                }
+                .modelContainer(SwiftDataStore.testStore.container)
+        }
+    }
+}
+
+#Preview(as: .systemLarge) {
+    TaskEmptyPreviewWidget()
+} timeline: {
+    TaskEntry(date: .now)
+}
+#endif
