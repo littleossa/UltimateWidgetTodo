be6689ed8d554ad384e98dcf0a2e1f74f3cd141f
littleossa <littleossa@gmail.com>
2023-11-07 06:28:47 +0900

    Add AlphabetModeKey

---
+++ UltimateWidgetTodoWidget/Views/Keyboard/Keys/AlphabetModeKey.swift
@@ -0,0 +1,55 @@
+//
+//  AlphabetModeKey.swift
+//  UltimateWidgetTodoWidgetExtension
+//
+//
+
+import AppIntents
+import SwiftUI
+
+struct AlphabetModeKey: View {
+            
+    var body: some View {
+        
+        Button(intent: AlphabetModeKeyIntent()) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 6)
+                    .fill(.keyShadow)
+                    .offset(y: 1)
+                
+                RoundedRectangle(cornerRadius: 6)
+                    .fill(.keyDarkGray)
+            }
+            .overlay {
+                Image(systemName: "abc")
+                    .font(.system(size: 12))
+                    .foregroundStyle(Color.label)
+            }
+        }
+    }
+}
+
+#Preview {
+    Color.keyboardBackground
+        .overlay {
+            AlphabetModeKey()
+        }
+}
+
+
+struct AlphabetModeKeyIntent: AppIntent {
+    
+    static var title: LocalizedStringResource = "Alphabet Mode key"
+    
+    @Parameter(title: "Alphabet Mode key")
+    var id: String
+    
+    init() {
+        id = "alphabetModekey"
+    }
+    
+    func perform() async throws -> some IntentResult {
+        KeyboardInputManager.shared.changeMode(into: .alphabet)
+        return .result()
+    }
+}
