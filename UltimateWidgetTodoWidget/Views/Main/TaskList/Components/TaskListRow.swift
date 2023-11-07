1403460b2de37b914ffffb83b9850d76e6a8abbf
littleossa <littleossa@gmail.com>
2023-11-08 08:22:58 +0900

    Update TaskListRow with AppIntent

--- UltimateWidgetTodoWidget/Views/Main/TaskList/Components/TaskListRow.swift
+++ UltimateWidgetTodoWidget/Views/Main/TaskList/Components/TaskListRow.swift
@@ -4,7 +4,9 @@
 //
 //
 
+import AppIntents
 import SwiftUI
+import WidgetKit
 
 struct TaskListRow: View {
     
@@ -16,11 +18,22 @@ struct TaskListRow: View {
             HStack {
                 TaskCompleteButton(task: task)
                 
-                Text(task.name)
-                    .font(.system(size: 16))
-                
-                Spacer()
+                Button(intent: TaskListRowSelectIntent(task: task)) {
+                    Rectangle()
+                        .fill(.widgetBackground.opacity(0.001))
+                        .overlay {
+                            
+                            HStack {
+                                Text(task.name)
+                                    .font(.system(size: 16))
+                                
+                                Spacer()
+                            }
+                        }
+                }
+                .buttonStyle(.plain)
             }
+            .frame(height: 24)
             .padding(.vertical, 8)
             
             Line()
@@ -29,5 +42,27 @@ struct TaskListRow: View {
                 .frame(height: 0.5)
                 .padding(.leading, 32)
         }
+    }
+}
+
+struct TaskListRowSelectIntent: AppIntent {
+    
+    static var title: LocalizedStringResource = "Todo List Row"
+    
+    @Parameter(title: "Todo Item ID")
+    var id: String
+    
+    init() {}
+    
+    init(task: Task) {
+        self.id = task.taskId.uuidString
+    }
+    
+    func perform() async throws -> some IntentResult {
+        if let uuid = UUID(uuidString: id) {
+            ScreenManager.shared.changeScreen(into: .editTask(id: uuid))
+        }
+        
+        return .result()
     }
 }
