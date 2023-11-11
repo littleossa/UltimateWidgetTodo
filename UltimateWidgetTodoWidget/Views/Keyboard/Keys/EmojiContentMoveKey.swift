8d29256212ef6708ef6171da61b0c2db4ff7d0bb
littleossa <littleossa@gmail.com>
2023-11-11 14:57:52 +0900

    Rename EmojiContentMoveKey direction

--- UltimateWidgetTodoWidget/Views/Keyboard/Keys/EmojiContentMoveKey.swift
+++ UltimateWidgetTodoWidget/Views/Keyboard/Keys/EmojiContentMoveKey.swift
@@ -10,45 +10,45 @@ import SwiftUI
 struct EmojiContentMoveKey: View {
     
     enum Direction {
-        case left
-        case right
+        case back
+        case forward
     }
     
     let type: Direction
     
     private var moveKeyIntent: any AppIntent {
         switch type {
-        case .left:
-            return EmojiContentMoveLeftKeyIntent()
-        case .right:
-            return EmojiContentMoveRightKeyIntent()
+        case .back:
+            return EmojiContentGoBackKeyIntent()
+        case .forward:
+            return EmojiContentGoForwardRightKeyIntent()
         }
     }
     
     var body: some View {
         
         Button(intent: moveKeyIntent) {
-            Image(systemName: type == .left ? "chevron.left" : "chevron.right")
+            Image(systemName: type == .back ? "chevron.left" : "chevron.right")
         }
     }
 }
 
 #Preview {
     HStack {
-        EmojiContentMoveKey(type: .left)
-        EmojiContentMoveKey(type: .right)
+        EmojiContentMoveKey(type: .back)
+        EmojiContentMoveKey(type: .forward)
     }
 }
 
-struct EmojiContentMoveLeftKeyIntent: AppIntent {
+struct EmojiContentGoBackKeyIntent: AppIntent {
     
-    static var title: LocalizedStringResource = "Emoji content move left key"
+    static var title: LocalizedStringResource = "Emoji content go back key"
     
-    @Parameter(title: "Emoji content move left key")
+    @Parameter(title: "Emoji content go back key")
     var id: String
     
     init() {
-        id = "emojiContentMoveLeftKey"
+        id = "emojiContentGoBackKey"
     }
     
     func perform() async throws -> some IntentResult {
@@ -58,15 +58,15 @@ struct EmojiContentMoveLeftKeyIntent: AppIntent {
 }
 
 
-struct EmojiContentMoveRightKeyIntent: AppIntent {
+struct EmojiContentGoForwardRightKeyIntent: AppIntent {
     
-    static var title: LocalizedStringResource = "Emoji content move right key"
+    static var title: LocalizedStringResource = "Emoji content go forward key"
     
-    @Parameter(title: "Emoji content move right key")
+    @Parameter(title: "Emoji content go forward key")
     var id: String
     
     init() {
-        id = "emojiContentMoveRightKey"
+        id = "emojiContentGoForwardKey"
     }
     
     func perform() async throws -> some IntentResult {
