6ea057eaaf4611542c0eb719ee1bd6f94660c8d8
littleossa <littleossa@gmail.com>
2023-11-12 14:30:21 +0900

    Update WidgetTodoView animation

--- UltimateWidgetTodoWidget/Widget/Contents/WidgetTodoView.swift
+++ UltimateWidgetTodoWidget/Widget/Contents/WidgetTodoView.swift
@@ -17,17 +17,61 @@ struct WidgetTodoView: View {
             switch core.currentScreen {
             case .main:
                 MainView()
+                    .transition(core.listPresentingTransition)
             case .addTask:
                 EditTaskView(type: .addNewTask)
+                    .transition(.push(from: .bottom))
             case let .editTask(id):
                 EditTaskView(type: .editTask(id: id))
+                    .transition(.push(from: .leading))
             }
         }
-        .contentTransition(.opacity)
         .modelContainer(core.swiftDataContainer)
     }
 }
 
 #Preview {
     WidgetTodoView()
+}
+
+struct WidgetTodoErrorView<Content: View>: View {
+    
+    init(error: WidgetError, @ViewBuilder content: () -> Content) {
+        self.content = content()
+        self.error = error
+    }
+    
+    let content: Content
+    let error: WidgetError
+    
+    var body: some View {
+        
+        ZStack {
+            content
+            
+            Color.black
+            
+            Rectangle()
+                .fill(.gray)
+            
+            VStack {
+                Text(error.info.title)
+                Text(error.info.message)
+            }
+        }
+    }
+}
+
+extension View {
+    
+    @ViewBuilder
+    func alert(widgetError: WidgetError?) -> some View {
+        if let widgetError {
+            WidgetTodoErrorView(error: widgetError) {
+                self
+            }
+        } else {
+            self
+        }
+    }
 }
