07171455b1cef760907bfd20c962b84ba569caf6
littleossa <littleossa@gmail.com>
2023-11-12 22:37:03 +0900

    Update WidgetError

--- UltimateWidgetTodoWidget/Model/Enum/WidgetError.swift
+++ UltimateWidgetTodoWidget/Model/Enum/WidgetError.swift
@@ -8,8 +8,9 @@ import SwiftUI
 
 enum WidgetError: String, Error {
     case unknown
-    case todoItemEditingFailure
     case todoItemDeletionFailure
+    case todoItemEditingFailure
+    case todoItemFetchingFailure
     case todoItemNameLimitExceeded
     
     struct Info {
@@ -24,16 +25,20 @@ enum WidgetError: String, Error {
             return .init(code: 0,
                          title: "Unknown Error",
                          message: "An unknown error occurred.")
-        case .todoItemEditingFailure:
-            return .init(code: 1,
-                         title: "Unable to edit the TODO item",
-                         message: "An error occurred while editing the item.")
         case .todoItemDeletionFailure:
-            return .init(code: 2,
+            return .init(code: 1,
                          title: "Unable to delete the TODO item",
                          message: "An error occurred while deleting the item.")
-        case .todoItemNameLimitExceeded:
+        case .todoItemEditingFailure:
+            return .init(code: 2,
+                         title: "Unable to edit the TODO item",
+                         message: "An error occurred while editing the item.")
+        case .todoItemFetchingFailure:
             return .init(code: 3,
+                         title: "Failed to Fetch TODO Items",
+                         message: "An error occurred while fetching TODO items.")
+        case .todoItemNameLimitExceeded:
+            return .init(code: 4,
                          title: "TODO Item Name Limit Exceeded",
                          message: "The item name exceeds the allowed character limit of \(WidgetConfig.todoItemNameLimitCount) characters. Please shorten this.")
         }
