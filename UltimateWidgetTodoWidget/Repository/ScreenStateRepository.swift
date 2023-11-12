96e77b04846b4e3e42e41ab2003945fbce0708c1
littleossa <littleossa@gmail.com>
2023-11-12 17:50:41 +0900

    Add set and reset error function in ScreenStateRepository

--- UltimateWidgetTodoWidget/Repository/ScreenStateRepository.swift
+++ UltimateWidgetTodoWidget/Repository/ScreenStateRepository.swift
@@ -18,7 +18,19 @@ class ScreenStateRepository {
         return store.screenType
     }
     
+    var error: WidgetError? {
+        return store.error
+    }
+    
     func changeScreen(into type: ScreenType) {
         store.screenType = type
+    }
+    
+    func setError(_ error: WidgetError) {
+        store.error = error
+    }
+    
+    func resetError() {
+        store.error = nil
     }
 }
